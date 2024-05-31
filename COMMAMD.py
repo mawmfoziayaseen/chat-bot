82de78044551d7354d9e5a2515e8075b6fcc40a3
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-05-31 18:02:47 +0500

    All files

---
+++ COMMAMD.py
@@ -0,0 +1,9 @@
+#Here are  all commands that run in terminal
+
+
+# pip install -- upgrade langchain
+# pip install langchain_community
+# pip install scikit_learn
+# pip install streamlit
+# pip install -r requirements.txt
+# streamlit run streamlit_app.py
